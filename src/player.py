e50c674e3b30658e0dfd934f8ab41046c8ef4ded
iacopo <iacopomarmorini@gmail.com>
2018-12-13 23:45:56 +0100

    Play only a region of 2 seconds; button to change it randomly
    The progress bar is broken when the region is not starting from 0
    
    Resolve #2

--- src/player.py
+++ src/player.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
+"""
+Play audio regions of a wav file.
+"""
 
 # Standard Library
+import random
 import sys
 import time
 import wave
@@ -16,6 +20,32 @@ from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow
 
 
+REG_SECONDS = 2
+
+
+def get_audio_output(params):
+    """
+    Create and return a QAudioOutput from wav params.
+    """
+    enc = 'audio/pcm'
+    fmt = QAudioFormat()
+    fmt.setChannelCount(params.nchannels)
+    fmt.setSampleRate(params.framerate)
+    fmt.setSampleSize(params.sampwidth * 8)
+    fmt.setCodec(enc)
+    fmt.setByteOrder(QAudioFormat.LittleEndian)
+    fmt.setSampleType(QAudioFormat.SignedInt)
+    return QAudioOutput(fmt)
+
+
+def read_wav_info(wav_path):
+    """
+    Read a wav file and return audio parameters
+    """
+    with wave.open(wav_path) as wav:
+        return wav.getparams()
+
+
 class MainWindow(QMainWindow):
     def __init__(self, wav_path):
         QMainWindow.__init__(self)
@@ -25,35 +55,22 @@ class MainWindow(QMainWindow):
         self._setLayout()
 
         self.wav_path = wav_path
+        self.params = read_wav_info(wav_path)
+        self.reg_nframes = int(self.params.framerate * REG_SECONDS)
+        self.duration = self.params.nframes / self.params.framerate
 
-        wav = wave.open(self.wav_path)
-        params = wav.getparams()
-        self.duration = params.nframes / params.framerate
-
-        start = 0
-        end = params.nframes
-        frametoread = end - start
-        wav.setpos(start)
-        self.data = wav.readframes(frametoread)
-        wav.close()
-        self._resetBuffer()
-
-        enc = 'audio/pcm'
-        fmt = QAudioFormat()
-        fmt.setChannelCount(params.nchannels)
-        fmt.setSampleRate(params.framerate)
-        fmt.setSampleSize(params.sampwidth * 8)
-        fmt.setCodec(enc)
-        fmt.setByteOrder(QAudioFormat.LittleEndian)
-        fmt.setSampleType(QAudioFormat.SignedInt)
-
-        self.output = QAudioOutput(fmt, self)
+        self.output = get_audio_output(self.params)
         self.output.stateChanged.connect(self.state_checkpoint)
         self.output.setNotifyInterval(20)
+
         self.output.notify.connect(self.notified)
         self.play_button.clicked.connect(self.play_pause)
+        self.random_button.clicked.connect(self.set_random_region)
 
         self.loop_enabled = False
+
+        self.buffer = QBuffer()
+        self.set_random_region()
 
     def _setLayout(self):
         widget = QtWidgets.QWidget()
@@ -65,16 +82,14 @@ class MainWindow(QMainWindow):
         self.progressBar.setTextVisible(True)
 
         self.play_button = QtWidgets.QPushButton('Play | Stop', widget)
+        self.random_button = QtWidgets.QPushButton('Random', widget)
 
         grid.addWidget(self.progressBar, 0, 0, 1, 3)
         grid.addWidget(self.play_button, 1, 1)
+        grid.addWidget(self.random_button, 1, 2)
 
         widget.setLayout(grid)
         self.setCentralWidget(widget)
-
-    def _resetBuffer(self):
-        self.buffer = QBuffer()
-        self.buffer.setData(self.data)
 
     def play(self):
         """
@@ -86,7 +101,6 @@ class MainWindow(QMainWindow):
                 self.output.stop()
             if sys.platform == 'darwin':
                 self.buffer.close()
-                self._resetBuffer()
                 self.buffer.open(QIODevice.ReadOnly)
             else:
                 # I found this way does not works on OS X
@@ -138,6 +152,28 @@ class MainWindow(QMainWindow):
         ts = tus / 1000000 - self.t_paused
         self.statusBar().showMessage('{:.3f}'.format(ts))
         self.progressBar.setValue(ts * 100 / self.duration)
+
+    def set_region(self, position):
+        """
+        Put the playback start position to `position`.
+        """
+        # FIXME: progress bar is broken when changing region
+        wav = wave.open(self.wav_path)
+        wav.setpos(position)
+        self.buffer.seek(0)
+        self.buffer.writeData(wav.readframes(self.reg_nframes))
+        wav.close()
+
+    def set_random_region(self):
+        """
+        Choose a random position and set playback start from there.
+        """
+        position = random.randrange(self.params.nframes - self.reg_nframes)
+        end = position + self.reg_nframes
+        print('Random region: {:.2f}-{:.2f}'.format(
+            position / self.params.framerate, end / self.params.framerate)
+        )
+        self.set_region(position)
 
 
 # cli args
